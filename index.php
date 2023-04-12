4bfb96a67088552bd78169a825212252a9722e41
kbykor <quddud33@gmail.com>
2023-04-12 19:33:53 +0900

    사용자 index에 로그인 링크 추가

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 include_once('./common.php');
-echo $_SESSION['mb_id']."님 환영합니다.";
+empty($_SESSION['mb_id']) || $mb_id = $_SESSION['mb_id']."님 환영합니다.";
 ?>
 <p>
+  <?php if(empty($mb_id)) {?>
+    <a href="<?=$base_URL?>sub/member/login.php">로그인 페이지로 이동하기</a><br/>
+    <?php } else { ?>
+    <?=$mb_id?><br/>
+  <a href="<?=$base_URL?>sub/member/logout.php">로그아웃</a><br/>
+  <?php } ?>
   <a href="<?=$base_URL?>adm/">관리자 페이지로 이동하기</a>
 </p>
