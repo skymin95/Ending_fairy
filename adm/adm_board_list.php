af57a2085a49fe1f695ce8ede3c78bf6a9346e38
skymin95 <minseok9036@naver.com>
2023-04-13 15:45:48 +0900

    게시판70퍼
    게시판70퍼

--- adm/adm_board_list.php
+++ adm/adm_board_list.php
@@ -1,86 +1,47 @@
 <?php
 $title = "게시판 관리"; // 타이틀
 include_once('./common.php');
+$mb_id = $_SESSION['mb_id']; // 회원명
 ?>
 <main>
-  <div class="tab_menu">
+<div class="tab_menu">
     <ul>
-      <li class="active"><a href="#tab1" title="공지사항">공지사항</a></li>
-      <li><a href="#tab2" title="이벤트">이벤트</a></li>
-      <li><a href="#tab3" title="커뮤니티">커뮤니티</a></li>
-      <a href="adm_board_insert.php" title="글쓰기" class="board_wp">글쓰기</a>
+      <li class="active"><a href="#tab1">공지사항</a></li>
+      <li><a href="#tab2">이벤트</a></li>
+      <li><a href="#tab3">커뮤니티</a></li>
+      <a href="adm_board_insert.php" class="board_wp">글쓰기</a>
     </ul>
 
     <div id="tab1" class="tab_content active">
-      <table>
+  
+    <table>
+      <tr>
         <th>번호</th>
         <th>제목</th>
         <th>글쓴이</th>
         <th>등록일</th>
         <th>관리</th>
+      </tr>
+      <?php
+
+$query = 'select * from board_notice order by notice_id desc'; //가장 최신글을 위로 올라오게 조회하여 데이터에 저장
+$result = mysqli_query($con, $query);
+      while($data = mysqli_fetch_array($result)){
+        $sql_member = "SELECT mb_no, mb_id, mb_name, mb_nick FROM member WHERE mb_no = '".$data['mb_no']."'";
+        $result_member = mysqli_query($con, $sql_member);
+        $row_member = mysqli_fetch_array($result_member);
+  ?>
         <tr>
-          <td>8</td>
-          <td><a href="" title="">4월 캐논아카데미 오프라인 강의 안내</a></td>
-          <td>관리자</td>
-          <td>23-03-26</td>
-          <td>
-            <button class="edit_btn"><a href="" title="수정">수정</a></button>
-            <button class="del_btn"><a href="" title="삭제">삭제</a></button>
-          </td>
-        </tr>
-        <tr>
-          <td>8</td>
-          <td><a href="" title="">4월 캐논아카데미 오프라인 강의 안내</a></td>
-          <td>관리자</td>
-          <td>23-03-26</td>
-          <td>
-            <button class="edit_btn"><a href="" title="수정">수정</a></button>
-            <button class="del_btn"><a href="" title="삭제">삭제</a></button>
-          </td>
-        </tr>
-        <tr>
-          <td>8</td>
-          <td><a href="" title="">4월 캐논아카데미 오프라인 강의 안내</a></td>
-          <td>관리자</td>
-          <td>23-03-26</td>
-          <td>
-            <button class="edit_btn"><a href="" title="수정">수정</a></button>
-            <button class="del_btn"><a href="" title="삭제">삭제</a></button>
-          </td>
-        </tr>
-        <tr>
-          <td>8</td>
-          <td><a href="" title="">4월 캐논아카데미 오프라인 강의 안내</a></td>
-          <td>관리자</td>
-          <td>23-03-26</td>
-          <td>
-            <button class="edit_btn"><a href="" title="수정">수정</a></button>
-            <button class="del_btn"><a href="" title="삭제">삭제</a></button>
-          </td>
-        </tr>
-        <tr>
-          <td>8</td>
-          <td><a href="" title="">4월 캐논아카데미 오프라인 강의 안내</a></td>
-          <td>관리자</td>
-          <td>23-03-26</td>
-          <td>
-            <button class="edit_btn"><a href="" title="수정">수정</a></button>
-            <button class="del_btn"><a href="" title="삭제">삭제</a></button>
-          </td>
-        </tr>
+        <td><?=$data['notice_id']?></td>
+        <td><a href="adm_board_view.php"><?=$data['notice_title']?></a></td>
+        <td><?= ($row_member['mb_nick'] == '' ? $row_member['mb_name'] : $row_member['mb_nick'])?></td>
+        <td><?=substr($data['mb_no'],0,10)?></td>
+        <td></td>
+      </tr>
+        <?php } ?>
       </table>
-      <div class="pagination">
-        <ul class="pagination">
-          <a class="prev" href="?page=1" title="prev"><i class="fa-solid fa-chevron-left"></i></a>
-          <li class="p_on"><a href="?page=1" title="">1</a></li>
-          <li><a href="?page=2" title="">2</a></li>
-          <li><a href="?page=3" title="">3</a></li>
-          <li><a href="?page=4" title="">4</a></li>
-          <li><a href="?page=5" title="">5</a></li>
-          <a class="next" href="?page=2" title="next"><i class="fa-solid fa-chevron-right"></i></a>
-        </ul>            
-      </div>
     </div>
+
     <div id="tab2" class="tab_content">
       탭 2 
     </div>
