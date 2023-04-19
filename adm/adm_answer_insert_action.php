9b04a391456d4d668b7f908a5a13caafe5bf02c6
kbykor <quddud33@gmail.com>
2023-04-19 09:51:16 +0900

    Update adm_answer_insert_action.php

--- adm/adm_answer_insert_action.php
+++ adm/adm_answer_insert_action.php
@@ -3,6 +3,12 @@ include_once('./common.php');
 
 $id = $_POST['question_id'];
 $content = $_POST['question_content'];
+$mb_id = $_SESSION['mb_id'];
+
+$sql_mb = "SELECT mb_no FROM member WHERE mb_id = '$mb_id'";
+$result_mb = mysqli_query($con, $sql_mb);
+$row_mb = mysqli_fetch_assoc($result_mb);
+$mb_no = $row_mb['mb_no'];
 
 $sql_parent = "SELECT * FROM `board_question` AS a INNER JOIN (SELECT question_parent_id FROM `board_question` WHERE question_parent_id IS NOT NULL) AS b ON b.question_parent_id = a.question_id WHERE question_id = '$id'";
 $result_parent = mysqli_query($con, $sql_parent);
@@ -12,7 +18,7 @@ if($row_parent != '0'){
   $sql = "UPDATE board_question SET question_content = '$content' WHERE question_parent_id = '$id'";
   $result = mysqli_query($con, $sql);
 }else{
-  $sql = "INSERT INTO board_question(question_parent_id, question_content, question_wdate, mb_no) VALUES('$id', '$content', now(), '')";
+  $sql = "INSERT INTO board_question(question_parent_id, question_content, question_wdate, mb_no) VALUES('$id', '$content', now(), '$mb_no')";
   mysqli_query($con, $sql);
 }
 
