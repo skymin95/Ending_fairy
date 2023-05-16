cbe270eae2942fb889401ea4e83d95890e96daca
skymin95 <minseok9036@naver.com>
2023-05-16 16:03:39 +0900

    공지사항 검색기능
    공지사항 검색기능

--- sub/notice/notice.search.php
+++ sub/notice/notice.search.php
@@ -4,8 +4,7 @@ include_once('../common.php');
 $mb_id = $_SESSION['mb_id']; // 회원명
 $page = empty($_GET['page']) ? 1 : $_GET['page']; // 현재페이지
 $cate = empty($_GET['cate']) ? 1 : $_GET['cate']; // 현재 카테고리
-$category = empty($_GET['category']) ? '' : $_GET['category']; // 현재 카테고리
-$search = empty($_GET['search']) ? '' : $_GET['search']; // 검색어
+$category = empty($_GET['category']) ? 1 : $_GET['category']; // 현재 카테고리
 
 switch($cate) {
   case '1': $cate_name = '공지사항'; $cate_table = 'notice'; break;
@@ -16,19 +15,17 @@ switch($cate) {
 ?>
 <main>
   <article id="notice_wrap">
-  <h2>
-    <a href="notice.php" title="공지사항">공지사항</a>
-  </h2>
+  <h2>공지사항</h2>
   <div class="notice_box">
   <ul class="notice_ul">
     <form name="notice" id="notice_form" method="get" action="notice.php">
       <?php
-      $sql_notice = "select * from board_notice ".($category != '' ? "WHERE $category LIKE '%$search%' " : "")." order by notice_id desc;";
+      $sql_notice = "select * from board_notice order by notice_id desc limit 8;";
       $result_notice= mysqli_query($con, $sql_notice);
 
         // 페이지내이션
         $num = mysqli_num_rows($result_notice);
-        $list_num = 8; /* paging : 한 페이지 당 데이터 개수 */
+        $list_num = 7; /* paging : 한 페이지 당 데이터 개수 */
         $page_num = 5; /* paging : 한 블럭 당 페이지 수 */
         // $page = isset($_GET["page"])? $_GET["page"] : 1; /* paging : 현재 페이지 */
         $total_page = ceil($num / $list_num); /* paging : 전체 페이지 수 = 전체 데이터 / 페이지당 데이터 개수, ceil : 올림값 */
@@ -44,7 +41,7 @@ switch($cate) {
         };
         $start = ($page - 1) * $list_num; /* paging : 시작 번호 = (현재 페이지 번호 - 1) * 페이지 당 보여질 데이터 수 */
 
-        $sql = "select * from board_notice ".($category != '' ? "WHERE $category LIKE '%$search%' " : "")." order by notice_id desc limit $start, $list_num"; /* paging : 쿼리 작성 - limit 몇번부터, 몇개 */
+        $sql = "select * from board_notice order by notice_id desc limit $start, $list_num"; /* paging : 쿼리 작성 - limit 몇번부터, 몇개 */
         $result_notice = mysqli_query($con, $sql); /* paging : 쿼리 전송 */
         $number = 0 + ($start);
   
@@ -72,7 +69,7 @@ switch($cate) {
             <option value="notice_title" <?=$category=="notice_title" ? "selected" : ""?>>제목</option>
             <option value="notice_content" <?=$category=="notice_content" ? "selected" : ""?>>내용</option>
           </select>
-          <input type="text" name="search" placeholder="SEARCH" value="<?=$search?>">
+          <input type="text" name="search" placeholder="SEARCH">
           <button type="submit" class="s_btn"><i class="fa-solid fa-magnifying-glass"></i></button>
         </div>
 
