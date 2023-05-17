f37ae3da0c1fe96c3a236eba41047d2627a947e7
xnclxud9 <xnclxud9@gmail.com>
2023-05-17 20:13:46 +0900

    1ㄷ1

--- sub/question/question_write.php
+++ sub/question/question_write.php
@@ -1,6 +1,26 @@
 <?php
 $title = "마이페이지 > 1:1문의 글쓰기"; // 타이틀
 include_once('../common.php');
+
+$mb_id = $_SESSION['mb_id']; // 회원명
+
+$id = (empty($_GET['question_id']) ? '' : $_GET['question_id']);
+$id = mysqli_real_escape_string($con, $id);
+if($id != '') {
+$query = "SELECT * FROM board_question WHERE question_id = '$id'";
+$result = mysqli_query($con, $query);
+$data = mysqli_fetch_assoc($result);
+} else {
+  $data = array(
+    'question_title' => '',
+    'question_content' => '',
+    'question_wdate' => '',
+  );
+}
+
+$sql_member = "SELECT mb_no, mb_id, mb_name, mb_nick FROM member WHERE mb_id = '".$_SESSION['mb_id']."'";
+$result_member = mysqli_query($con, $sql_member);
+$row_member = mysqli_fetch_array($result_member);
 ?>
 <script type="text/javascript" src="<?=$base_URL?>se2/js/HuskyEZCreator.js" charset="utf-8"></script>
 <main>
@@ -11,12 +31,13 @@ include_once('../common.php');
   1:1문의
   </a>
 </h2>
-<form name="question_write" id="question_w_form" method="post" action="">
+<form name="question_write" id="question_w_form" method="post" action="question_write_action.php">
 
 <article class="question_write_box">
-<input type="text" placeholder="제목을 입력하세요." class="q_write_input">
+  <h3>공지사항</h3>
+<input type="text" name="question_title" placeholder="제목을 입력하세요." class="q_write_input">
 
-<textarea name="editorTxt" id="editorTxt" rows="10" cols="100" style="width: 100%;min-width:260px;" class="q_editor"></textarea>
+<textarea name="question_content" id="editorTxt" rows="10" cols="100" style="width: 100%;min-width:260px;" class="q_editor"></textarea>
 <script id="smartEditor" type="text/javascript"> 
 /* 에디터 설정 */
 let oEditors = [];
@@ -28,6 +49,13 @@ smartEditor = function() {
         sSkinURI: "<?=$base_URL?>se2/SmartEditor2Skin.html",
         fCreator: "createSEditor2"
     })
+    let w_form = document.querySelector('#question_w_form');
+    w_form.addEventListener('submit', function(e){
+      e.preventDefault();
+      oEditors.getById["editorTxt"].exec("UPDATE_CONTENTS_FIELD", []);
+      console.log('dd');
+      this.submit();
+    });
 }
 smartEditor();
 </script>
@@ -40,10 +68,13 @@ smartEditor();
     <input type="submit" value="등록">
   </li>
 </ul>
+</form>
 
 
 
 
 </article>
-
 </main>
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/Ending_fairy/footer.php');
+?>
